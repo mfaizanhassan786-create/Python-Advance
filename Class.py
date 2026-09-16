0f92b48225c234c7d109b9744b60f45f8739cdb1
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-16 23:31:39 +0500

    update

--- Class.py
+++ Class.py
@@ -2,22 +2,47 @@
 #In this file we are disscuss about the Problems.
 
 
+
+# class Car:
+#     def drive(self, name):
+#         print(f"Driving {name}")
+
+
+# car1 = Car()
+# car1.drive("BMW")
+
 class Car:
-    def __init__(self, color, name):
-        self.name = name
-        self.color = color
+    color = "red"
+    def drive (self):
+        print(f"{self.color} car is moving!")
+
+
+car1 = Car()
+
+car1.drive()
+
+
+
+
+# class Car:
+#     def __init__(self, color, name):
+#         self.name = name
+#         self.color = color
     
-    def start_engine(self):
-        print(f"Engine started {self.name} and {self.color}")
+#     def start_engine(self):
+#         print(f"Engine started {self.name} and {self.color}")
 
-# First object of the class Car
-car1 = Car("red", "BMW")
-car2 = Car("blue", "Audi")
+# # First object of the class Car
+# car1 = Car("red", "BMW")
+# car2 = Car("blue", "Audi")
 
-car1.start_engine()
-car2.start_engine()
+# car1.start_engine()
+# car2.start_engine()
 
-# Second Object
+# # Second Object
 
-myfriend = Car("green", "Toyota")
-myfriend.start_engine()
+# myfriend = Car("green", "Toyota")
+# myfriend.start_engine()
+
+
+

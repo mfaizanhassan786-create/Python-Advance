a2ae7d6c61810c39e57b4f5c2e0203d700df94e5
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-17 19:11:37 +0500

    update

--- Inharitance.py
+++ Inharitance.py
@@ -14,3 +14,18 @@
 # childdog = Dog()
 # childdog.speak()
 # childdog.bark()
+
+
+# #Mothod Overriding:
+
+
+# class Animal:
+#     def speak(self):
+#         print("Some sound is made")
+
+# class Dog(Animal):
+#     def speak(self):
+#         print("Woof")
+        
+# dog1 = Dog()
+# dog1.speak()

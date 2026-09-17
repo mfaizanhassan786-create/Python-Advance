09846a762d998b578a2259926f0d43f851c3a969
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-17 18:00:09 +0500

    updating

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,21 +1,45 @@
-#Encapluation in python
+# #Encapluation in python
 
-class Student:
-    def __init__(self, age, name, grade, course):
-        self.age = age
-        self.name = name
-        self.__grade = grade # Private Attribute
-        self.course = course
+# class Student:
+#     def __init__(self, age, name, grade, course):
+#         self.age = age
+#         self.name = name
+#         self.__grade = grade # Private Attribute
+#         self.course = course
 
-    # To access the private attribute we use getters and setters
-    def get_grade(self):
-        return self.__grade
+#     # To access the private attribute we use getters and setters
+#     def get_grade(self):
+#         return self.__grade
 
-    def set_grade(self, grade):
-        self.__grade = grade
+#     def set_grade(self, grade):
+#         self.__grade = grade
 
-student1 = Student(20, "Faizan", "A", "Computer Science")
-# print(student1.__grade) # It will give error as the grade is private attribute
+# student1 = Student(20, "Faizan", "A", "Computer Science")
+# # print(student1.__grade) # It will give error as the grade is private attribute
 
-# Accessing private attribute through getter
-print("Student Grade:", student1.get_grade())
+# # Accessing private attribute through getter
+# print("Student Grade:", student1.get_grade())
+
+#Another Example:
+
+# class Student:
+#     @property
+#     def grade(self):
+#         return self.__grade
+
+#     def __init__(self, age, name, grade, course):
+#         # self.age = age
+#         # self.name = name
+#         self.__grade = grade # Private Attribute
+#         # self.course = course
+#     def get_grade(self):
+#         return self.__grade
+#     def set_grade(self, grade):
+#         if 0 <= grade <= 100:
+#             self.__grade = grade
+#         else:
+#             print("Invalid grade")
+
+# student1 = Student(20, "Faizan", "A", "Computer Science")
+# student1.set_grade(90)
+# print(student1.get_grade())

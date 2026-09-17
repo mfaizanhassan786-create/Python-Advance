422c96ec285c689816abc5c4f398912ad84f6ec7
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-17 17:55:54 +0500

    updating

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,10 +1,21 @@
-
 #Encapluation in python
 
 class Student:
-    def __init__ (self, age, name, grade, course):
-        self.__grade = grade #Private Attribute
+    def __init__(self, age, name, grade, course):
+        self.age = age
+        self.name = name
+        self.__grade = grade # Private Attribute
+        self.course = course
+
+    # To access the private attribute we use getters and setters
+    def get_grade(self):
+        return self.__grade
+
+    def set_grade(self, grade):
+        self.__grade = grade
+
 student1 = Student(20, "Faizan", "A", "Computer Science")
 # print(student1.__grade) # It will give error as the grade is private attribute
 
-# To access the private attribute we use getters and setters
+# Accessing private attribute through getter
+print("Student Grade:", student1.get_grade())

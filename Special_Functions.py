cfc0bf28a568d4528edf1df6464d0c21352bd289
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-17 23:08:02 +0500

    updating

--- Special_Functions.py
+++ Special_Functions.py
@@ -1,17 +1,39 @@
-# Special Fnctions:
-# Methods
-# Functions
+# # Special Fnctions:
+# # Methods
+# # Functions
 
-class Book:
-    def __init__(self,title,author,pages):
-        self.title = title
-        self.author = author
-        self.pages = pages
+# class Book:
+#     def __init__(self,title,author,pages):
+#         self.title = title
+#         self.author = author
+#         self.pages = pages
 
-    def __str__(self):
-        return f"Title: {self.title}\nAuthor: {self.author}\nPages: {self.pages}"
+#     def __str__(self):
+#         return f"Title: {self.title}\nAuthor: {self.author}\nPages: {self.pages}"
 
-b = Book("Python", "Faizan", 100)
-print(b)
+# b = Book("Python", "Faizan", 100)
+# print(b)
     
     
+#Another Example:
+class Playlist:
+    def __init__(self, name, songs):
+        self.name = name
+        self.songs = songs
+
+    def __len__(self):
+        return len(self.songs)
+
+    def __add__(self, other):
+        # Combines both playlist names and merges both song lists
+        return Playlist(self.name + " & " + other.name, self.songs + other.songs)
+
+# FIXED: Added names ("Chill Mix" and "Gym Beats") as the first argument
+p1 = Playlist("Chill Mix", ["Song1", "Song2", "Song3"])
+p2 = Playlist("Gym Beats", ["Song4", "Song5", "Song6"])
+
+# This prints the number of songs in p1 (3)
+print(len(p1))
+
+# This merges p1 and p2 using the __add__ method, then counts all songs (6)
+print(len(p1 + p2))

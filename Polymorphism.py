b29ab9d2e96d5d3cf9616a085ccd00f9f61280c9
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-17 19:27:23 +0500

    updating

--- Polymorphism.py
+++ Polymorphism.py
@@ -1,10 +1,47 @@
-class Dog:
-    def speak(self):
-        print("Woof!")
 
-class Cat:
-    def speak(self):
-        print("Meow!")
+# Polymorphism basically means
+# that a function can take more than one form. Like a single function could be used
+# 
 
-for pet in [Dog(), Cat()]:
-    pet.speak()
+# class Dog:
+#     def speak(self):
+#         print("Woof!")
+
+# class Cat:
+#     def speak(self):
+#         print("Meow!")
+
+# for pet in [Dog(), Cat()]:
+#     pet.speak()
+
+
+# Another Example:
+
+# class vehicle:
+#     def move(self):
+#         print("some movement is done")
+
+# class car(vehicle):
+#     def move(self):
+#         print("car is moving on road")
+
+# class boat(vehicle):
+#     def move(self):
+#         print("Boat is moving on water")
+
+# car1 = car()
+# boat1 = boat()
+
+
+# Another Example:
+
+class Calculator:
+    def add(self, a, b=0, c=0):
+        return a+b+c
+
+cal = Calculator()
+print(cal.add(5))
+print(cal.add(10,20))
+print(cal.add(10,20,30))
+
+

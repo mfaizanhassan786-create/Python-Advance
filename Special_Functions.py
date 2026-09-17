662f113ac077b179fdd41d7f8e56a7db76d99ea6
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-17 23:13:24 +0500

    update

--- Special_Functions.py
+++ Special_Functions.py
@@ -15,25 +15,47 @@
 # print(b)
     
     
-#Another Example:
-class Playlist:
-    def __init__(self, name, songs):
-        self.name = name
-        self.songs = songs
+# #Another Example:
+# class Playlist:
+#     def __init__(self, name, songs):
+#         self.name = name
+#         self.songs = songs
 
-    def __len__(self):
-        return len(self.songs)
+#     def __len__(self):
+#         return len(self.songs)
 
-    def __add__(self, other):
-        # Combines both playlist names and merges both song lists
-        return Playlist(self.name + " & " + other.name, self.songs + other.songs)
+#     def __add__(self, other):
+#         # Combines both playlist names and merges both song lists
+#         return Playlist(self.name + " & " + other.name, self.songs + other.songs)
 
-# FIXED: Added names ("Chill Mix" and "Gym Beats") as the first argument
-p1 = Playlist("Chill Mix", ["Song1", "Song2", "Song3"])
-p2 = Playlist("Gym Beats", ["Song4", "Song5", "Song6"])
+# # FIXED: Added names ("Chill Mix" and "Gym Beats") as the first argument
+# p1 = Playlist("Chill Mix", ["Song1", "Song2", "Song3"])
+# p2 = Playlist("Gym Beats", ["Song4", "Song5", "Song6"])
 
-# This prints the number of songs in p1 (3)
-print(len(p1))
+# # This prints the number of songs in p1 (3)
+# print(len(p1))
 
-# This merges p1 and p2 using the __add__ method, then counts all songs (6)
-print(len(p1 + p2))
+# # This merges p1 and p2 using the __add__ method, then counts all songs (6)
+# print(len(p1 + p2))
+
+
+
+# #Another Example:
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def __str__(self):
+#         return f"({self.x}, {self.y})"
+
+#     def __add__(self, other):
+#         if not isinstance(other, Point):
+#             return TypeError("Operand must be a Point")
+#         return Point(self.x + other.x, self.y + other.y)
+
+# p1 = Point(1, 2)
+# p2 = Point(3, 4)
+
+# print((p1 + p2).x, (p1 + p2).y)

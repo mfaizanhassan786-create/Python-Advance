12cbe5e26063dbde8166c57ce0baa8c11b2ed881
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-18 22:15:43 +0500

    Updating

--- File_Handling/read_writeing.py
+++ File_Handling/read_writeing.py
@@ -1,7 +1,17 @@
 # In this file we are discuss about the Reading and writing the data.
 
-file = open("File_Handling/demo.txt", "r")
+## Reading Formating 
 
-print(file.read())
+# file = open("File_Handling/demo.txt", "r")
+
+# print(file.read())
+
+# file.close()
+
+## writing Formating
+
+file = open("demo.txt", "w")
+
+file.write("Hello, this is a new line")
 
 file.close()

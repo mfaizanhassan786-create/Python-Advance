b72b2671507e1a8aa1107fb1a57228b7f0b8ff09
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-18 22:36:48 +0500

    updaing

--- File_Handling/read_writeing.py
+++ File_Handling/read_writeing.py
@@ -17,6 +17,51 @@
 # file.close()
 
 
-file1 = open("demo.txt", "a")
-file1.write("\nHello, this is second new line and I am writing this line using a")
-file1.close()
+## Appending Formating:
+
+# file1 = open("demo.txt", "a")
+
+# file1.write("\nHello, this is second new line and I am writing this line using a")
+
+# file1.close()
+
+
+## Context Manager:
+
+## open method:
+
+# with open("demo.txt", "r") as file:
+
+#     print(file.read())
+# # It is closing the file automatically.
+
+## tempfile method:
+
+# import tempfile
+
+# temp_file = tempfile.NamedTemporaryFile(mode="w", delete=False)
+
+# temp_file.write("Hello, this is a new line")
+
+# temp_file.close()
+
+
+## Zipfile Method:
+
+# import zipfile
+
+# with zipfile.ZipFile("demo.zip", "w") as zip_file:
+
+#     zip_file.write("demo.txt")
+
+#     print("File zipped successfully!")
+
+
+## pathlib method:
+
+from pathlib import Path
+
+path = Path("demo.txt")
+
+path.write_text("Hello, this is a new line")
+ 

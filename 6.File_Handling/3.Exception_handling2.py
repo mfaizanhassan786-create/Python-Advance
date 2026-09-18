ca1f68a4aaea167e613be782cd7f1f0cb9966791
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-18 23:15:26 +0500

    Adding the new file.

--- 6.File_Handling/3.Exception_handling2.py
+++ 6.File_Handling/3.Exception_handling2.py
@@ -7,23 +7,28 @@
 
 ## Using try-except:
 
-try:
-    num = int(input("Enter a number: "))
-    print(10/num)
-except ZeroDivisionError:
-    print("Cannot divide by zero")
-except ValueError:
-    print("Invalid input")
-except Exception as e:
-    print(f"An unexpected error occurrred : {e}")
+# try:
+#     num = int(input("Enter a number: "))
+#     print(10/num)
+# except ZeroDivisionError:
+#     print("Cannot divide by zero")
+# except ValueError:
+#     print("Invalid input")
+# except Exception as e:
+#     print(f"An unexpected error occurrred : {e}")
 
 
-try:
-    print("Running code...")
-except:
-    print("Error Found!!!")
-else:
-    print("Code is running successfully")
-finally:
-    print("Code is terminated")
+# try:
+#     print("Running code...")
+# except:
+#     print("Error Found!!!")
+# else:
+#     print("Code is running successfully")
+# finally:
+#     print("Code is terminated")
     
+
+
+age = -3
+if age < 0:
+    raise ValueError("Age cannot be negative")

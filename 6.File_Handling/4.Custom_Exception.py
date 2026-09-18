e099813ed59d5ed49d3e5846fb78fcc22a5fbc00
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-18 23:20:25 +0500

    Updating

---
+++ 6.File_Handling/4.Custom_Exception.py
@@ -0,0 +1,16 @@
+
+
+# class InvalidAgeError(Exception):
+#     pass
+
+# age = int(input("Enter Your Age: "))
+ 
+# if age <0:
+#     raise InvalidAgeError(f"Age cannot be negative!, {age}")
+
+# try:
+#     age = int(input("Enter Your Age: "))
+#     if age <0:
+#       raise InvalidAgeError(f"Age cannot be negative!, {age}")
+# except InvalidAgeError as e:
+#     print(e)
